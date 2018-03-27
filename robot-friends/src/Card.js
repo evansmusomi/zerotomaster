8fcabaccb8cfc718923756503357659a6d9161d8
Evans Musomi <evans.musomi@gmail.com>
2018-03-27 17:57:48 +0300

    Adds Initial Card Component and Removes Unused Files

---
+++ robot-friends/src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img src="" alt="robots" />
+      <div>
+        <h2>Harvey Specter</h2>
+        <p>harvey@specter.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
