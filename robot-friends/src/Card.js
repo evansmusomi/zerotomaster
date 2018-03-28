cdfca0c5714eb562d693cc2dfaae3f8543c713b5
Evans Musomi <evans.musomi@gmail.com>
2018-03-28 02:08:54 +0000

    Adds Styled Card Component and Seed Data

--- robot-friends/src/Card.js
+++ robot-friends/src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({id, name, email, picture}) => {
   return (
-    <div>
-      <img src="" alt="robots" />
+    <div className="bg-light-green dib pa3 br3 ma2 grow bw2 shadow-5">
+      <img src={`${picture}`} width="200" height="200" alt={`${name}`} />
       <div>
-        <h2>Harvey Specter</h2>
-        <p>harvey@specter.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
--- robot-friends/src/index.js
+++ robot-friends/src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Card from "./Card";
+import { robots } from "./robots";
+import "tachyons";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Card />, document.getElementById("root"));
+ReactDOM.render(<div>
+                    <Card id={ robots[0].id } name={robots[0].name} email={robots[0].email} picture={robots[0].picture} />
+                    <Card id={ robots[1].id } name={robots[1].name} email={robots[1].email} picture={robots[1].picture} />
+                    <Card id={ robots[2].id } name={robots[2].name} email={robots[2].email} picture={robots[2].picture} />
+                    <Card id={ robots[3].id } name={robots[3].name} email={robots[3].email} picture={robots[3].picture} />
+                    <Card id={ robots[4].id } name={robots[4].name} email={robots[4].email} picture={robots[4].picture} />
+                    <Card id={ robots[5].id } name={robots[5].name} email={robots[5].email} picture={robots[5].picture} />
+                </div>, document.getElementById("root"));
 registerServiceWorker();
