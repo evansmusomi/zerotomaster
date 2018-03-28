575f8fcdc5c7f723d5eb2005eb3c218ee8bf61b8
Evans Musomi <evans.musomi@gmail.com>
2018-03-28 07:42:09 +0300

    Adds SearchBox Component and Updates to README

---
+++ suits-cast/src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <div className="pa2">
+      <input
+        type="search"
+        className="pa3 ba b--green bg-lightest-blue"
+        place="Search Suits"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
